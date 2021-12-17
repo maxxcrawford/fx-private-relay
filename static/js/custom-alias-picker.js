6b62b1b03c82ef4bd9f2885a445f9e12ebe93593
Vincent <git@vincenttunru.com>
2021-12-17 13:50:25 +0100

    WIP: UI for custom alias picker, content still pending

--- static/js/custom-alias-picker.js
+++ static/js/custom-alias-picker.js
@@ -1,54 +1,20 @@
-/* global postProfileSubdomain */
+/* global apiRequest */
 
 (function() {
 	"use strict";
 
     const customAliasPicker = {
         init: () => {
-            const triggerButton = document.querySelector(".js-dash-create-new-domain-dialog-trigger");
+            const triggerButton = document.querySelector(".js-dash-create-new-domain-alias-dialog-trigger");
             if (triggerButton) {
                 triggerButton.addEventListener("click", customAliasPicker.modal.open, false);  
             }
         },
-        // checkIfDomainIsSafeAndAvailable: async (domain)=> {
-        //     if (!domain) {
-        //         throw new Error("No domain submitted.");
-        //     }
-            
-        //     const requestUrl = `/accounts/profile/subdomain?subdomain=${domain}`;
-
-        //     const response = await fetch(requestUrl, {
-        //         method: "get",
-        //         mode: "same-origin",
-        //         credentials: "same-origin",
-        //     });
-
-        //     // Catch Redirect Failure
-        //     // If the endpoint returns false on a queried subdomain, it means one of two things: 
-        //     //      1. The domain is already registered
-        //     //      2. The domain is on the emails/badwords.txt 
-        //     // 
-        //     // If we know their request is going to fail, we stop trying to catch the submission and let it fail. 
-        //     // This will add a messages cookie with the "Domain Not Available" error and reload the page. 
-        //     // if (response.redirected) {
-        //     //     return false;
-        //     // }
-
-        //     if (!response.ok) {
-        //         return false;
-        //         // const message = `An error has occured: ${response.status}`;
-        //         // throw new Error(message);
-        //     }
-
-        //     const status = await response.json();
-
-        //     return status.available;
-        // }, 
         canModalBeSubmitted: ()=>{
-            const modalConfirmCheckbox = document.querySelector(".js-modal-domain-registration-confirmation-checkbox");
-            const modalSubmit = document.querySelector(".js-modal-domain-registration-submit");
+            const modalPrefixInput = document.getElementById("customAliasPrefix");
+            const modalSubmit = document.querySelector(".js-modal-custom-alias-picker-submit");
 
-            if (!modalConfirmCheckbox.checked) {
+            if (modalPrefixInput.value.length === 0) {
                 modalSubmit.disabled = true;
                 return false;
             }
@@ -56,30 +22,6 @@
             modalSubmit.disabled = false;
             return true;
         },
-        // events: {
-        //     onSubmit: async (e) => {
-        //         e.preventDefault();
-
-        //         const currentForm = e.target;
-        //         const requestedDomain = currentForm.querySelector(".js-subdomain-value").value.toLowerCase();
-        //         currentForm.querySelector(".js-subdomain-value").value = requestedDomain;
-        //         const domainCanBeRegistered = await customAliasPicker.checkIfDomainIsSafeAndAvailable(requestedDomain);               
-        //         const messages = document.querySelector(".js-notification");
-        //         const messagesHtml = document.querySelector(".js-notification-html");
-
-        //         if (domainCanBeRegistered) {
-        //             // Dismiss error state if visible
-        //             messages?.classList.add("is-hidden"); 
-        //             messagesHtml?.classList.add("is-hidden");
-        //             e.target.classList.remove("mzp-is-error");
-        //             customAliasPicker.modal.open(e.target);
-        //         } else {
-        //             // If the domain cannot be registered, add error state to the form, and display an error message.
-        //             e.target.classList.add("mzp-is-error")
-        //             customAliasPicker.showError(requestedDomain);
-        //         }
-        //     }
-        // },
         modal: {
             /**
              * @param {MouseEvent} triggeringClickEvent 
@@ -90,16 +32,16 @@
                 modal.classList.add("is-visible");
 
 
-                const modalCancel = document.querySelector(".js-modal-domain-registration-cancel");
+                const modalCancel = document.querySelector(".js-modal-custom-alias-picker-cancel");
                 modalCancel.addEventListener("click", customAliasPicker.modal.close, false);
 
-                const modalSubmit = document.querySelector(".js-modal-domain-registration-submit");
+                const modalSubmit = document.querySelector(".js-modal-custom-alias-picker-submit");
                 modalSubmit.disabled = true;
                 modalSubmit.addEventListener("click", customAliasPicker.modal.formSubmitRequest, false);
 
-                const modalConfirmCheckbox = document.querySelector(".js-modal-domain-registration-confirmation-checkbox");
-                modalConfirmCheckbox.checked = false;
-                modalConfirmCheckbox.addEventListener("change", customAliasPicker.canModalBeSubmitted, false);
+                const modalPrefixInput = document.getElementById("customAliasPrefix");
+                modalPrefixInput.value = "";
+                modalPrefixInput.addEventListener("input", customAliasPicker.canModalBeSubmitted, false);
 
                 // Close the modal if the user clicks outside the modal
                 modal.addEventListener("click", (e) => {
@@ -117,7 +59,7 @@
                     return;
                 }   
                 
-                const modal = document.querySelector(".js-modal-domain-registration-confirmation");
+                const modal = document.querySelector(".js-modal-custom-alias-picker");
                 modal.classList.remove("is-visible");
 
                 document.removeEventListener("keydown", customAliasPicker.modal.close, false);
@@ -125,75 +67,29 @@
             },
             formSubmitRequest: async (e) => {
                 
-                const modalConfirmCheckbox = document.querySelector(".js-modal-domain-registration-confirmation-checkbox");
+                const modalPrefixInput = document.getElementById("customAliasPrefix");
 
-                if (!modalConfirmCheckbox.checked) {
+                if (modalPrefixInput.value.length === 0) {
                     return false;
                 }
 
-                e.target.parentFormHTMLElement.removeEventListener("submit", customAliasPicker.events.onSubmit, false);  
-
-                const formSubmission = await postProfileSubdomain({
-                    "form": e.target.parentFormHTMLElement, 
-                    "domain": e.target.parentFormRequestedDomain
+                const response = await apiRequest("/domainaddresses/", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        enabled: true,
+                        address: modalPrefixInput.value,
+                    }),
                 });
-
-                if (formSubmission.status !== "Accepted") {
-                    throw new Error();
-                }
-                
-                document.getElementById("mpp-choose-subdomain").classList.add("is-hidden");
-                
-                switch (e.target.parentFormHTMLElement.id) {
-                    case "domainRegistration":
-                        customAliasPicker.modal.showSuccessState(e.target.parentFormRequestedDomain,{ "form": "dashboard" });
-                        break;
-                    case "onboardingDomainRegistration":
-                        customAliasPicker.modal.showSuccessState(e.target.parentFormRequestedDomain, { "form": "onboarding" });
-                        break;
+                if (!response.ok) {
+                    showError();
+                } else {
+                    // Display the new alias:
+                    document.location.reload();
                 }
                 
             },
-            showSuccessState: (domain, {form})=> {
-                const modalRegistrationForm = document.querySelector(".js-custom-alias-picker-form");
-                const modalRegistrationSuccessState = document.querySelector(".js-domain-registration-success");
-                modalRegistrationForm.classList.add("is-hidden");
-                modalRegistrationSuccessState.classList.remove("is-hidden");
-
-                const domainPreview = document.querySelector(".js-premium-onboarding-domain-registration-preview");
-                domainPreview.textContent = domain + ".mozmail.com";
-
-                const modalContinue = document.querySelector(".js-modal-domain-registration-continue");
-
-                if (form === "onboarding") {
-                    modalContinue.addEventListener("click", customAliasPicker.modal.close, false);
-                    
-                    const onboardingDomainRegistration = document.querySelector(".js-premium-onboarding-domain-registration-form");
-                    onboardingDomainRegistration.classList.add("is-hidden");
-                    onboardingDomainRegistration.nextElementSibling.classList.add("is-visible");
-
-                    const dashboardDomainRegistrationPrompt = document.querySelector(".mpp-dashbaord-header-action");
-                    dashboardDomainRegistrationPrompt?.classList.add("is-hidden");
-
-                    const onboardingDomainRegistrationActionButtons = document.querySelectorAll(".c-premium-onboarding-action-step-2 button");
-                    onboardingDomainRegistrationActionButtons.forEach( button => {
-                        button.classList.toggle("is-hidden");
-                    });
-                }
-
-                if (form === "dashboard") {
-                    modalContinue.addEventListener("click", ()=>{
-                        location.reload();
-                    }, false);
-                }
-
-
-
-
- 
-            }
         },
-        showError: (requestedDomain) => {
+        showError: () => {
             const messagesDjango = document.querySelector(".js-notification");
             const messages = document.querySelector(".js-notification-html");
             const messageWrapper = messages.querySelector(".message-wrapper");
@@ -206,8 +102,7 @@
             messageWrapper.classList.add("error");
 
             // Grab the translated message, replace the domain placeholder with requested domain and post the message. 
-            let message = messageFluent.dataset.errorSubdomainNotAvailable;
-            message = message.replace("REPLACE", requestedDomain); 
+            let message = messageFluent.dataset.errorAliasCreation;
             messageWrapperSpan.textContent = message;
 
             const messagesDismissButton = document.querySelector(".js-dismiss-html");
@@ -216,25 +111,6 @@
             });
 
         },
-        showSuccess: (requestedDomain) => {
-            const messages = document.querySelector(".js-notification");
-            const messageWrapper = messages.querySelector(".message-wrapper");
-            const messageWrapperSpan = messageWrapper.querySelector("span");
-            const messageFluent = messages.querySelector("fluent");
-            messages.classList.remove("is-hidden");
-            messageWrapper.classList.remove("error");
-            messageWrapper.classList.add("success");
-
-            // Grab the translated message, replace the domain placeholder with requested domain and post the message. 
-            let message = messageFluent.dataset.successSubdomainRegistered;
-            message = message.replace("REPLACE", requestedDomain); 
-            messageWrapperSpan.textContent = message;
-
-            // Reload the page
-            setTimeout(()=>{
-                location.reload(); 
-            }, 2000);
-        }
     }
 
     document.addEventListener("DOMContentLoaded", ()=>{
